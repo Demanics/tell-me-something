29764f8c059d619d6dad878e10ae0397703ff10f
demanics <itsmani.pk@gmanil.com>
2026-06-06 11:58:57 +0500

    Setup the server.

---
+++ server/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from pydantic_models.chat_body import ChatBody
+from services.search_service import SearchService;
+
+app=FastAPI()
+search_service=SearchService()
+
+# chat
+@app.post("/chat")
+def chat_endpoint(body:ChatBody):
+    # search web and find the appropriate sources
+    search_service.web_search(body.query)
+    # sort the sources
+    # generate response using the LLM
+    return body.query
---
+++ server/services/search_service.py
@@ -0,0 +1,7 @@
+from config import Settings
+
+settings=Settings()
+
+class SearchService:
+    def web_search(self, query:str):
+       print(Settings().TAVILY_API_KEY)
